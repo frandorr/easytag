a2317456fb16bca4a73feaa515be55411ff71fa5
Fran Dorr <fran.dorr@gmail.com>
2019-10-27 23:26:45 -0300

    First commit

---
+++ easytag/core.py
@@ -0,0 +1,35 @@
+class Tagger(object):
+    """docstring for Tagger."""
+
+    def __init__(self, reader=None, preprocessor=None, model=None, postprocessor=None):
+        super(Tagger, self).__init__()
+        self._reader = reader
+        self._preprocessor = preprocessor
+        self._model = model
+        self._postprocessor = postprocessor
+
+    def tag(self, doc):
+        return self._model.tag(doc)
+
+
+    def run(self):
+        """
+        Run tagger on corpus.
+
+        Apply preprocessors (if any), models (if any) and postprocessors (if any)
+        to given corpus.
+
+        Returns:
+            type: description
+
+        Raises:
+            Exception: description
+
+        """
+        corpus = self.reader.read()
+
+        if self._preprocessor:
+            corpus = self._preprocessor.preprocess(corpus)
+
+        tags = [self._model.tag(item) for item in corpus]
+        
